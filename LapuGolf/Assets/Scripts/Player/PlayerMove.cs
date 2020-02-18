a7daac25f7b7fb35d52cafd02bf18282e4f55e92
LLapuras <chenwanwan13@gmail.com>
2020-02-18 02:54:40 +0000

    add gettotalmass function
    get total mass of a certain rigidbody and its children.

--- LapuGolf/Assets/Scripts/Player/PlayerMove.cs
+++ LapuGolf/Assets/Scripts/Player/PlayerMove.cs
@@ -17,7 +17,15 @@ public class PlayerMove : MonoBehaviour
     private Vector3 direction;
     private Vector3 pos;
     private float force;
+
     private Scene currentScene;
+
+    public float strength = 0;
+    public Image powerBar;
+    public float delta = 50;
+    public float maxPower = 20000;
+
+    public float totalMass;
 
     Vector3 dir;
     private void Start()
@@ -29,9 +37,12 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        //KeyboardRPG();//for testing
-        PointShoot();
+        KeyboardRPG();//for testing
+        PointShoot02();
+        //PointShoot();
         isOutofArea();
+        //PressJumpForward();
+        //PressJumpUp();
     }
 
 
@@ -51,10 +62,10 @@ public class PlayerMove : MonoBehaviour
 
         if (Input.GetKey("d"))
             rb.AddForce(Camera.main.transform.right * speed * 10);
-
-        //if (Input.GetKey("space"))
+        
+        //if (Input.GetKeyDown("space"))
         //{
-        //    rb.AddForce(Vector3.up * jumpspeed * 5);
+        //    rb.AddForce(Vector3.up * jumpspeed * 10);
         //}
 
     }
@@ -90,10 +101,112 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
+    void PointShoot02()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.rigidbody.tag == "Ground" || hit.rigidbody.tag == "Block")
+                {
+
+                    if (strength <= maxPower)
+                    {
+                        strength += delta;
+                    }
+                    else
+                    {
+                        strength = maxPower;
+                    }
+                    direction = pos - rb.transform.position;
+                    direction.y = 0;
+                    dirline.positionCount = 2;
+                    dirline.SetPosition(0, rb.transform.position);
+                    dirline.SetPosition(1, rb.transform.position + direction.normalized * 5);
+                    pos = hit.point;
+                    pos.y = rb.transform.position.y; //avoid jumpping
+                    force = Mathf.Min(Vector3.Distance(pos, rb.transform.position), maxthrust) * thrust;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {         
+            dirline.positionCount = 0;
+            rb.AddForce(direction.normalized * strength * 0.3f);
+        }
+    }
+
+
+
+    void PressJumpForward()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (strength <= maxPower)
+            {
+                strength += delta;
+            }
+            else
+            {
+                strength = maxPower;
+            }
+            powerBar.GetComponent<RectTransform>().sizeDelta = new Vector2(20, strength/maxPower * 500);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            rb.AddForce(Camera.main.transform.forward * strength);
+            strength = 0;
+        }
+    }
+
+    void PressJumpUp()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (strength <= maxPower)
+            {
+                strength += delta;
+            }
+            else
+            {
+                strength = maxPower;
+            }
+            powerBar.GetComponent<RectTransform>().sizeDelta = new Vector2(20, strength / maxPower * 500);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            rb.AddForce(new Vector3(0,1,0) * strength);
+            strength = 0;
+        }
+    }
+
+
+    float GetTotalMass(GameObject ob)
+    {
+        float ma = 0;
+        ma += ob.GetComponent<Rigidbody>().mass;
+
+        Rigidbody[] bodies;
+        bodies = GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody x in bodies)
+        {
+            ma += x.mass;
+        }
+
+        return ma;
+    }
+
 
     void isOutofArea()
     {
-        if(transform.position.y < -15)
+        if(transform.position.y < -20)
         {
             Destroy(gameObject);
             SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
