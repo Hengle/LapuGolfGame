097771740522f5f2557635e3809f15cdb974c691
LLapuras <chenwanwan13@gmail.com>
2020-02-22 18:42:24 +0000

    map editor update

---
+++ LapuGolf/Assets/Scripts/MapGenerator/MapGenerator.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public GameObject go;
+    public int GridX = 5;
+    public int GridY = 5;
+    public Vector3 OriginPos = new Vector3(0, 0, 0);
+    public GameObject[,] Tiles;
+    public GameObject[] MenuBtns;
+    public Object[] Blocks;
+
+    public GameObject curGo;
+
+    private float offsetX = 0;
+    private float offsetY = 0;
+    private float offsetZ = 0;
+
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tiles = new GameObject[GridX, GridY];
+
+        LoadBlockMenu();
+
+        curGo = Blocks[0] as GameObject;
+
+        if (go == null)
+        {
+            go = Resources.Load("GeneratedCube") as GameObject;
+        }
+        else if (go != null)
+        {
+            OriginPos = go.transform.position;
+        }
+
+        offsetX = go.transform.localScale.x;
+        offsetY = go.transform.localScale.y;
+        offsetZ = go.transform.localScale.z;
+
+        for (int i = 0; i < GridX; i++)
+        {
+            for (int j = 0; j < GridY; j++)
+            {
+                Debug.Log(i);
+                Tiles[i, j] = Instantiate(go, OriginPos + new Vector3(i * offsetX, 0, j * offsetZ), go.transform.rotation);
+                Tiles[i, j].transform.SetParent(transform);
+            }
+        }
+    }
+
+    void LoadBlockMenu()
+    {
+        Blocks = Resources.LoadAll("Prefab/Gameplay/Ground");
+        MenuBtns = new GameObject[Blocks.Length];
+        GameObject btnPrefab = Resources.Load("Prefab/Gameplay/UI/MenuBtn") as GameObject;
+        int i = 0;
+        foreach (Object ob in Blocks)
+        {
+            MenuBtns[i] = Instantiate(btnPrefab) as GameObject;
+            MenuBtns[i].name = "Blocks" + i.ToString();
+            MenuBtns[i].transform.GetChild(0).GetComponent<Text>().text = ob.name;
+            MenuBtns[i].transform.SetParent(GameObject.Find("Menu").transform);
+            MenuBtns[i].GetComponent<Button>().onClick.AddListener(() => GetCurGO(ob));
+            i++;
+        }
+    }
+
+    public void GetCurGO(Object ob)
+    {
+        Debug.Log(ob.name);
+        curGo = Resources.Load("Prefab/Gameplay/Ground/" + ob.name) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+    }
+}
---
+++ LapuGolf/Assets/Scripts/MapGenerator/Tile.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    public Color originColor;
+    public Color mouseonColor;
+    public Color selectColor;
+
+    public GameObject ob;
+    public GameObject building;
+    private Vector3 newpos;
+    private bool selected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = originColor;
+        newpos = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selected == true)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = selectColor;
+        }
+        else if (selected == false)
+        {
+
+        }
+
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Block")
+            {
+                Debug.Log("emmmmmm");
+            }
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButton(0) && building == null)
+        {
+            selected = true;
+            building = Instantiate(gameObject.transform.GetComponentInParent<MapGenerator>().curGo, newpos, transform.rotation) as GameObject;
+        }
+        else if (Input.GetMouseButton(1) && building != null) 
+        {
+            selected = false;
+            Destroy(building);
+            Debug.Log("awsl");
+        }
+        
+        gameObject.GetComponent<SpriteRenderer>().color = mouseonColor;
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = originColor;
+    }
+
+}
