9cd9e8c997da9eaf7dd2805f6ce3893ad3881ae8
LLapuras <chenwanwan13@gmail.com>
2020-02-23 21:01:17 +0000

    update all

--- LapuGolf/Assets/Scripts/MapGenerator/MapGenerator.cs
+++ LapuGolf/Assets/Scripts/MapGenerator/MapGenerator.cs
@@ -6,21 +6,32 @@ using UnityEngine.UI;
 public class MapGenerator : MonoBehaviour
 {
 
-    public GameObject go;
+    enum Direction { origin, left, back, right };
+
+    public GameObject grid;
     public int GridX = 5;
     public int GridY = 5;
     public Vector3 OriginPos = new Vector3(0, 0, 0);
     public GameObject[,] Tiles;
     public GameObject[] MenuBtns;
     public Object[] Blocks;
+    
+    public GameObject curObPos;
 
     public GameObject curGo;
+    Direction curGoDir = Direction.origin;
+    int dirIndex=0;
+    private float roty = 0;
+    private float rotz = 0;
 
     private float offsetX = 0;
     private float offsetY = 0;
     private float offsetZ = 0;
 
     private Button button;
+
+
+    public Vector3 mousepos;
 
     // Start is called before the first frame update
     void Start()
@@ -31,30 +42,30 @@ public class MapGenerator : MonoBehaviour
 
         curGo = Blocks[0] as GameObject;
 
-        if (go == null)
-        {
-            go = Resources.Load("GeneratedCube") as GameObject;
-        }
-        else if (go != null)
-        {
-            OriginPos = go.transform.position;
-        }
+        GenerateGrid();
+    }
 
-        offsetX = go.transform.localScale.x;
-        offsetY = go.transform.localScale.y;
-        offsetZ = go.transform.localScale.z;
+    void GenerateGrid()
+    {
+        OriginPos = grid.transform.position;
+        Debug.Log(OriginPos);
+
+        offsetX = grid.transform.localScale.x;
+        offsetY = grid.transform.localScale.y;
+        offsetZ = grid.transform.localScale.z;
 
         for (int i = 0; i < GridX; i++)
         {
             for (int j = 0; j < GridY; j++)
             {
                 Debug.Log(i);
-                Tiles[i, j] = Instantiate(go, OriginPos + new Vector3(i * offsetX, 0, j * offsetZ), go.transform.rotation);
+                Tiles[i, j] = Instantiate(grid, OriginPos + new Vector3(i * offsetX, 0, j * offsetZ), grid.transform.rotation);
                 Tiles[i, j].transform.SetParent(transform);
             }
         }
     }
 
+    //generate block menu button
     void LoadBlockMenu()
     {
         Blocks = Resources.LoadAll("Prefab/Gameplay/Ground");
@@ -72,16 +83,109 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
+    //detect buttonclick event
     public void GetCurGO(Object ob)
     {
-        Debug.Log(ob.name);
         curGo = Resources.Load("Prefab/Gameplay/Ground/" + ob.name) as GameObject;
+        Vector3 mouseObPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        curGo = Instantiate(curGo, mouseObPos, curGo.transform.rotation);
+
+      
     }
 
     // Update is called once per frame
     void Update()
     {
+        PlaceObjOnMap();
+        mouseOnGrid();
+        //RotateOb();
+    }
 
-       
+    //place curgo on grid
+    void PlaceObjOnMap()
+    {
+        Vector3 mouseObPos = Input.mousePosition;
+
+        Ray castPoint = Camera.main.ScreenPointToRay(mouseObPos);
+        RaycastHit hit;
+        if (Physics.Raycast(castPoint, out hit, 100))
+        {
+            mouseObPos = hit.point;
+            mouseObPos.y = 0.5f;
+
+            curGo.transform.position = mouseObPos;
+        }
+
+        //rotate before placing
+    }
+
+    void RotateOb()
+    {
+        Quaternion newRot = curGo.transform.rotation;
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            dirIndex++;
+            roty += 90;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            dirIndex--;
+            roty -= 90;
+        }
+
+        int dirIndex0 = Mathf.Abs(dirIndex) % 4;
+
+        switch (dirIndex0)
+        {
+            case 1:
+                curGoDir = Direction.left;
+                newRot = Quaternion.Euler(-90, roty, 0);
+                break;
+            case 2:
+                curGoDir = Direction.back;
+                newRot = Quaternion.Euler(-90, roty, 0);
+                break;
+            case 3:
+                curGoDir = Direction.right;
+                newRot = Quaternion.Euler(-90, roty, 0);
+                break;
+            case 0:
+                curGoDir = Direction.origin;
+                newRot = Quaternion.Euler(-90, roty, 0);
+                break;
+        }
+
+        Debug.Log(newRot);
+        curGo.transform.localRotation = newRot;
+
+        curObPos.transform.GetChild(0).GetComponent<Text>().text = curGo.name;
+        curObPos.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<InputField>().text = Mathf.Round(curGo.transform.position.x).ToString();
+        curObPos.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<InputField>().text = Mathf.Round(curGo.transform.position.y).ToString();
+        curObPos.transform.GetChild(0).GetChild(2).GetChild(0).GetComponent<InputField>().text = Mathf.Round(curGo.transform.position.z).ToString();
+
+        curObPos.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = curGoDir.ToString();
+    }
+
+    void mouseOnGrid()
+    {
+
+        RaycastHit objectHit;
+        if (Physics.Raycast(curGo.transform.position, transform.forward, out objectHit, 50))
+        {
+            Debug.Log("Raycast hitted to: " + objectHit.collider);
+        }
+
+    }
+
+    //obselete
+    void PlaceObjOnMap02()
+    {
+        mousepos = Input.mousePosition;
+        mousepos.z = 10;
+        mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+        mousepos.y = 0.5f;
+
+        curGo.transform.position = mousepos;
     }
 }
